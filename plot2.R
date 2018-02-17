cf417495f76e13fb81619d7b1ebfa6a1f21b2c2d
Kent Lanclos <35151480+KentLanclos@users.noreply.github.com>
2018-02-17 06:27:59 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -7,6 +7,6 @@ datasub <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(datasub$Date, datasub$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 activepower <- as.numeric(datasub$Global_active_power)
 png("plot2.png", width=480, height=480)
-plot(datetime, globalactivepower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+plot(datetime, activepower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
 
